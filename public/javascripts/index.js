a0a9103b2ee4d769891f24e23353c4d2cef7c0e0
tscully49 <thomas1374@yahoo.com>
2015-12-01 16:50:06 -0600

    project done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,57 +4,57 @@ var ready = false;
 
 $(document).ready(function() {
   var calculatedHeight = parseInt($('body').height()) - parseInt($('#2').outerHeight());
-  var socket = io();
+  var socket = io(); // connects the socket to the server 
 
   $('.form-inline').css('width', $(document).find('.container').css('width'));
   $('#msgs, #people').css('height', calculatedHeight - $('#div-title').outerHeight(true) - 3);
-  $('#chat').hide();
+  $('#chat').hide(); // hide the chat until the user is logged in 
 
   $("form").submit(function(event){
-    event.preventDefault();
+    event.preventDefault(); // prevents the page from refreshing when you submit a request
   });
 
-  $("#lgn").keypress(function(e){
-    if(e.which == 13) {
+  $("#lgn").keypress(function(e){ // when the user hits the enter key when typing a request 
+    if(e.which == 13) { // the key for enter 
       var msg = $("#lgn").val();
-      if (msg.trim()) {
+      if (msg.trim()) { // trim the message of trailing whitespaces 
         socket.emit("join", msg);
         $("#lgn").val("");
       }
     }
   });
 
-  $("#login").click(function(){
+  $("#login").click(function(){ // when someone clicks the login button 
     var msg = $("#lgn").val();
-    if (msg.trim()) {
+    if (msg.trim()) { // trim trailing whitespaces
       socket.emit("join", msg);
       $("#lgn").val("");
     }
   });
 
-  socket.on("loginError", function(message) {
+  socket.on("loginError", function(message) { // when there is a login error, output the message 
     $('#msgs').append($('<li class="message-list">').html("<div class='error-text'>" + message + "</div>"))
     console.log(message);
   });
 
-  socket.on("logOn", function(messages) {
+  socket.on("logOn", function(messages) { // when the user successfully logs in, hide the login form and show the chats 
     $('#join').hide();
     $('#chat').show();
 
-    jQuery.each(messages, function(i, message) {
+    jQuery.each(messages, function(i, message) {  // load all public messages that have already been posted to the user's screen
       if (message.type === "message") {
         $("#msgs").append($('<li class="message-list">').html("<div class='message-text'>" + message.name + ": " + message.message + "</div>"));
       } else {
-        $("#msgs").append($('<li class="updated-person">').html(message.name + message.message));
+        $("#msgs").append($('<li class="updated-person">').html(message.name + message.message)); 
       }
     });
-    $("#msgs").scrollTop($('#msgs')[0].scrollHeight);
+    $("#msgs").scrollTop($('#msgs')[0].scrollHeight); // scroll to the end of the list so you see the most recent message 
 
-    ready = true;
+    ready = true; // set variable
   });
 
 
-  $("#send").click(function(){
+  $("#send").click(function(){ // when a user clicks the send button 
     var msg = $("#msg").val();
     if (msg.trim()) {
       socket.emit("send", msg);
@@ -62,10 +62,10 @@ $(document).ready(function() {
     }
   });
 
-  $("#msg").keypress(function(e){
+  $("#msg").keypress(function(e){ // when a user hits the enter key when typing in the message box 
     if(e.which == 13) {
       var msg = $("#msg").val();
-      if (msg.trim()) {
+      if (msg.trim()) { // trim message and send to the server 
         socket.emit("send", msg);
         $("#msg").val("");
       }
@@ -73,13 +73,13 @@ $(document).ready(function() {
   });
 
   socket.on("update", function(msg) {
-    if(ready) {
+    if(ready) { // when someone leaves or enters the room and the user is logged in 
       $("#msgs").append($('<li>').text(msg).addClass("updated-person"));
       $("#msgs").scrollTop($('#msgs')[0].scrollHeight);
     }
   });
 
-  socket.on("update-people", function(people){
+  socket.on("update-people", function(people){ // when someone enters or leaves the room, only runs if the user is logged in 
     if(ready) {
       $("#people").empty();
       $.each(people, function(clientid, person) {
@@ -88,35 +88,35 @@ $(document).ready(function() {
     }
   });
 
-  socket.on("chat", function(person, msg){
+  socket.on("chat", function(person, msg){ // when the request is successful, the user will append it to the list 
     if(ready) {
       $("#msgs").append($('<li class="message-list">').html(" <div class='message-text'>" + person + ": " + msg + "</div>"));
       $("#msgs").scrollTop($('#msgs')[0].scrollHeight);
     }
   });
 
-  socket.on("privateChat", function(person, msg){
+  socket.on("privateChat", function(person, msg){ // when the request is successful and the message is private, append the message 
     if(ready) {
       $("#msgs").append($('<li class="message-list">').html(" <div class='message-text'>" + person + " (to you): " + msg + "</div>"));
       $("#msgs").scrollTop($('#msgs')[0].scrollHeight);
     }
   });
 
-  socket.on("who", function(msg){
+  socket.on("who", function(msg){ // who all is in the room, append to the list of messages 
     if(ready) {
       $("#msgs").append($('<li class="message-list">').html(" <div class='message-text'>" + msg + "</div>"));
       $("#msgs").scrollTop($('#msgs')[0].scrollHeight);
     }
   });
 
-  socket.on("disconnect", function(){
+  socket.on("disconnect", function(){ // when the server disconnects 
     $("#msgs").append("The server is not available.  Please refresh the page");
     $("#msg").attr("disabled", "disabled");
     $("#lgn").attr("disabled", "disabled");
     $("#send").attr("disabled", "disabled");
   });
 
-  socket.on("disconnected", function() {
+  socket.on("disconnected", function() { // when the user chooses to logout and the server approves 
     socket.disconnect();
     $("#msg").attr("disabled", "disabled");
     $("#lgn").attr("disabled", "disabled");
--- sockets/base.js
+++ sockets/base.js
@@ -7,91 +7,92 @@ var messages = [];
 
 module.exports = function (io) {
 
+	// Event listener for when a client connects to the server
 	io.on('connection', function (client) {
 
-		client.emit("show", knownAccounts);
-
+		// When a client attempts to connect to the chat room
   	client.on('join', function(profile) {
+  		// Take their request and break it up into strings
   		var loginString = profile.split(" ");
   		var loggedIn = false;
 
+  		// Check to make sure this user is not already logged in
   		for (var person in people) {
   			if (people[person] === loginString[1]) {
   				loggedIn = true;
   			}
   		}
 
+  		// Error check the login, check for the correct length of request
   		if (loginString.length != 3) {
   			client.emit("loginError", "Incorrect login format, please use ex. 'login (username) (password)'");
   			return;
-  		} else if (loginString[0] != "login") {
+  		} else if (loginString[0] != "login") { // make sure the first word in the requets is "login"
   			client.emit("loginError", "Denied!  Please login first");
   			return;
-  		} else if (knownAccounts[loginString[1]] == undefined) {
+  		} else if (knownAccounts[loginString[1]] == undefined) { // make sure the name is under the list of known users
   			client.emit("loginError", "Username does not exists");
   			return;
-  		} else if (knownAccounts[loginString[1]].Password != loginString[2]) {
+  		} else if (knownAccounts[loginString[1]].Password != loginString[2]) { // make sure the password is correct
   			client.emit("loginError", "Password is incorrect");
   			return;
-  		} else if (numClients >= 3) {
+  		} else if (numClients >= 3) {	// make sure that too many people aren't logged in 
   			client.emit("loginError", "Too many people are logged in, please wait for someone to leave");
   			return;
-  		} else if (knownAccounts[loginString[1]].Password == loginString[2] && loggedIn === false) {
+  		} else if (knownAccounts[loginString[1]].Password == loginString[2] && loggedIn === false) { // if the user is not already logged in and the password is correct
   			numClients = numClients + 1;
-  			console.log(numClients);
   			console.log(loginString[1] + " logged in.");
   			var cachedMessages = {"name": loginString[1], "message": " has joined the server.", "type": "connection"};
-      	messages.push(cachedMessages);
+      	messages.push(cachedMessages);  // cache the message 
 
-  			people[client.id] = loginString[1];
-  			client.broadcast.emit('update', loginString[1] + " has joined the server.");
+  			people[client.id] = loginString[1]; // set the username to the socket.id
+  			client.broadcast.emit('update', loginString[1] + " has joined the server."); // log to the server what happened
   			client.emit("logOn", messages);
   		} else {
-  			client.emit("loginError", "This user is already logged in");
+  			client.emit("loginError", "This user is already logged in"); // the user is already logged in 
   			return;
   		}
 
-  		io.emit('update-people', people);
+  		io.emit('update-people', people); // add the person to the list of people in the room 
 
-  		client.on("send", function(message){
-  			var msg = message.split(" ");
-  			if (msg.length === 1) {
+  		client.on("send", function(message){ // when a user sends a request after being logged in 
+  			var msg = message.split(" "); // split up the message into seperate strings 
+  			if (msg.length === 1) { // if there is only one string, check to make sure it's either "who" or "logout" otherwise, error
 		  		if (msg[0] === "who") {
 	  				var peopleArray = "";
 	  				for(var key in people) {
 	  					peopleArray = peopleArray + people[key] + ", ";
 	  				}
-	  				console.log(people);
 	  				peopleArray = peopleArray.slice(0, -2);
 	        	client.emit("who", peopleArray);
 	    		} else if (msg[0] === "logout") {
-	    			client.emit("disconnected");
+	    			client.emit("disconnected");  // disconnect from the server
 	    		} else {
 			    	client.emit("loginError", "Invalid command input.  Please try again");
 	    		}
-		    } else if (msg.length >= 3) {
+		    } else if (msg.length >= 3) { // If the request has three or more strings in it, make sure they are known commands, otherwise error out and don't do anything else 
 		    	if (msg[0] === "send") {
 		    		var thisMessage = "";
 
-		    		for (var i = 2; i < msg.length; i++) {
+		    		for (var i = 2; i < msg.length; i++) { // the first two strings tell you "send" and who to send to, then take the rest of the strings as the message body 
 		    			thisMessage = thisMessage + msg[i] + " ";
 		    		}
 
-		    		if (msg[1] === "all") {
+		    		if (msg[1] === "all") { // emits to all clients 
 		    			var cachedMessages = {"name": people[client.id], "message": thisMessage, "type": "message"};
 		    			messages.push(cachedMessages);
 		    			console.log(people[client.id] + ": " + thisMessage);
 		    			io.emit("chat", people[client.id], thisMessage);
-		    		} else {
+		    		} else { // ELSE emits to only the person that was specified
 		    			var socketId = in_array(people, msg[1]);
-		    			if (socketId != false) {
+		    			if (socketId != false) { // checks to make sure that the user specified exists before it sends the message 
 		    				var cachedMessages = {"name": people[client.id] + "(to " + msg[1] + "): " , "message": thisMessage, "type": "message"};
-		    				messages.push(cachedMessages);
-		    				console.log(people[client.id] + "(to " + msg[1] + "): " + thisMessage);
+		    				//messages.push(cachedMessages);  // cache the message 
+		    				console.log(people[client.id] + "(to " + msg[1] + "): " + thisMessage);  // log the message to the server 
 			    			client.emit("chat", people[client.id] + "(to " + msg[1] + ")", thisMessage);
-			    			io.sockets.connected[socketId].emit("privateChat", people[client.id], thisMessage);
+			    			io.sockets.connected[socketId].emit("privateChat", people[client.id], thisMessage); // send the message to the client 
 			    		} else {
-			    			client.emit("loginError", "The user you want to talk to does not exist in this room");
+			    			client.emit("loginError", "The user you want to talk to does not exist in this room");  // different errors 
 			    		}
 			    	}
 		    	} else {
@@ -103,23 +104,23 @@ module.exports = function (io) {
     	});
 		
 			client.on('disconnect', function(){
-				numClients = numClients - 1;
+				numClients = numClients - 1; // if the user is joined and disconnects, reduce the number of clients connected to the server
 			});
   	});
   	
-  	client.on('disconnect', function(){
+  	client.on('disconnect', function(){ // if the user disconnects from the server
 	    if (typeof people[client.id] != 'undefined') {    	
-	    	io.emit('update', people[client.id] + " has left the server.");
+	    	io.emit('update', people[client.id] + " has left the server."); 
 	    	var cachedMessages = {"name": people[client.id], "message": " has left the server.", "type": "connection"};
       		console.log(cachedMessages.name + cachedMessages.message);
-      		messages.push(cachedMessages);
+      		messages.push(cachedMessages); // cache the message 
 	    }
 	    delete people[client.id];
-	    io.emit('update-people', people);
+	    io.emit('update-people', people); // update the people in the room 
 		});	
 	});
 
-	function in_array(array, name) {
+	function in_array(array, name) { // check if the value is in the array 
 	    for(var i in array) {
 	      if (array[i] === name) {
 	       	return i;
